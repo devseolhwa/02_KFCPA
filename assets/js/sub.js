b7aacd03a0db8febe75a76a01f4c4cc711ce82d2
Seolhwa <devseolhwa@gmail.com>
2024-05-30 19:04:55 +0900

    서브

--- assets/js/sub.js
+++ assets/js/sub.js
@@ -1,8 +1,10 @@
 $(function(){
     
     //lnb
-	$("#subVisual .lnb li.location02").on("click", function(){
-		$(this).toggleClass("active");
-		$(this).find(".depth02").stop().slideToggle();
+	$("#subVisual .lnb li button").on("click", function(){
+		$(this).parent("li").toggleClass("active").siblings("li").removeClass("active");
+		$(this).next(".depth02").stop().slideToggle();
+		$(this).parents().siblings("li").find(".depth02").slideUp();
 	});
+
 });
